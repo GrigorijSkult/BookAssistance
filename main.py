8c8a459af6f33e87119068d3435bbcb560f08f35
Gkos <grigorijs.sk@gmail.com>
2022-09-26 21:00:24 +0200

    Price read logic

--- main.py
+++ main.py
@@ -4,44 +4,42 @@ import requests
 from bs4 import BeautifulSoup, ResultSet
 import re
 
-url = 'https://yoomoney.ru/checkout/payments/v2/contract/bankcard?orderId=2ac2a21c-000f-5000-8000-15697ec7c852'
+url = 'https://yoomoney.ru/checkout/payments/v2/contract/bankcard?orderId=2ac40155-000f-5000-9000-150055c1d0cf'
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
 
-quotes1 = soup.find_all("span", class_="Text__StyledTextSpan-sc-9bqqn7-0")
-quotes2: ResultSet[Any] = soup.find_all(string=re.compile("Оплата заказа №"))# window.__data #orderId
-
-#print(quotes2)
+quotes: ResultSet[Any] = soup.find_all(string=re.compile("Оплата заказа №"))# window.__data #orderId
 
 paragraphs: list[str] = []
-for x in quotes2:
+for x in quotes:
     paragraphs.append(str(x))
 
-first = paragraphs[0].find("№") + 1
-orderNumber = paragraphs[0][first:first+8]
-orderPrice = 0
+#order number
+orderNumber = ""
+firstDigit = paragraphs[0].find("№") + 1
+orderText = paragraphs[0][firstDigit:firstDigit + 30]
+for x in orderText:
+    if x == '"':
+        break
+    else:
+        orderNumber += x
+
+#order Price
+orderPrice = ""
+orderCurrency = ""
+firstDigit = paragraphs[0].find("totalPrice" + '"' ) + 13
+priceText = paragraphs[0][firstDigit:firstDigit + 30]
+i=0
+for x in priceText:
+    if x == '"':
+        orderCurrency = paragraphs[0][firstDigit + i + 18 : firstDigit + i + 18 + 3]
+        i = 0
+        break
+    else:
+        i+=1
+        orderPrice += x
+
 
 print(orderNumber)
-
-
-
-
-
-
-#print(quotes1)
-#print(soup.prettify())
-
-#https://istories.media/workshops/2021/09/10/parsing-s-pomoshchyu-python-urok-1/
-#https://tproger.ru/articles/tips-and-libraries-for-web-scraping/
-
-# /html/body/div[3]/div/div[1]/div[1]/div/div[2]/div/div/span
-#'<span class="Text__StyledTextSpan-sc-9bqqn7-0 jOxTDb">Оплата заказа №19012351</span>
-
-
-#<span class="Text__StyledTextSpan-sc-9bqqn7-0 dZyMnu">150</span>
-#/html/body/div[3]/div/div[1]/div[1]/div/div[1]/span/span/span[1]
-
-
-
-
-
+print(orderPrice)
+print(orderCurrency)
