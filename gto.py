87fb41766bc63a23eff3772e283d89edc372e5c2
Gkos <grigorijs.sk@gmail.com>
2022-10-05 22:14:30 +0200

    Price read logic #2

--- gto.py
+++ gto.py
@@ -5,5 +5,6 @@ class BookInfo:
         self.order_price = order_price
         self.order_currency = order_currency
 
-    def info(self):
-        print(self.order_number,  self.order_price,  self.order_currency)
+class ErrorsInfo(Exception):
+    def __int__(self, txt):
+        self.txt = txt
